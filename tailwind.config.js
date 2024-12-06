8bff50a94bcc803bf7ede7cc33f76535eef2c81a
anhtuan512002 <anhtuan51202@gmail.com>
2024-12-06 23:32:44 +0700

    change font jestbrainsMono

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,14 +27,7 @@ module.exports = {
 				DEFUALT:'#00ff99',
 				hover:'#00e187',
 			},
-  			background: 'var(--background)',
-  			foreground: 'var(--foreground)'
   		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
   	}
   },
   plugins: [require("tailwindcss-animate")],
